333abc43feca7a86ebb56426ee45f8612957fc8a
Mika Andrianarijaona <mika@tag-ip.com>
2016-11-16 18:23:53 +0300

    feat: add props to menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
 const Menu = props => (
   <div>
@@ -8,9 +9,9 @@ const Menu = props => (
           <a href="#home" className="navbar-brand">Mobilab Gallery</a>
         </div>
         <ul className="nav navbar-nav">
-          <li role="presentation" className=""><a href="#hot">Hot</a></li>
-          <li role="presentation" className=""><a href="#top">Top</a></li>
-          <li role="presentation" className=""><a href="#user">User</a></li>
+          <li role="presentation" className={props.galleryType === 'HOT' ? 'active' : null}><a href="#hot">Hot</a></li>
+          <li role="presentation" className={props.galleryType === 'TOP' ? 'active' : null}><a href="#top">Top</a></li>
+          <li role="presentation" className={props.galleryType === 'USER' ? 'active' : null}><a href="#user">User</a></li>
         </ul>
       </div>
     </nav>
@@ -18,6 +19,11 @@ const Menu = props => (
 );
 
 Menu.propTypes = {
+  galleryType: PropTypes.oneOf('HOT', 'TOP', 'USER'),
 };
 
-export default Menu;
+const mapStateToProps = state => ({
+  galleryType: state.gallery.type,
+});
+
+export default connect(mapStateToProps)(Menu);
