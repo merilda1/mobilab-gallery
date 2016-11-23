4fb38a55cca08cdb75ab6d2900d1a543d505c962
Mika Andrianarijaona <mika@tag-ip.com>
2016-11-23 13:55:11 +0300

    add viral option when switching gallery

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,9 +10,9 @@ const Menu = props => (
           <a href="#home" className="navbar-brand">Mobilab Gallery</a>
         </div>
         <ul className="nav navbar-nav">
-          <li role="presentation" className={props.galleryType === 'HOT' ? 'active' : null}><a href="#hot" onClick={() => props.changeGalleryType('HOT')}>Hot</a></li>
-          <li role="presentation" className={props.galleryType === 'TOP' ? 'active' : null}><a href="#top" onClick={() => props.changeGalleryType('TOP')}>Top</a></li>
-          <li role="presentation" className={props.galleryType === 'USER' ? 'active' : null}><a href="#user" onClick={() => props.changeGalleryType('USER')}>User</a></li>
+          <li role="presentation" className={props.galleryType === 'HOT' ? 'active' : null}><a href="#hot" onClick={() => props.changeGalleryType('HOT', props.viral)}>Hot</a></li>
+          <li role="presentation" className={props.galleryType === 'TOP' ? 'active' : null}><a href="#top" onClick={() => props.changeGalleryType('TOP', props.viral)}>Top</a></li>
+          <li role="presentation" className={props.galleryType === 'USER' ? 'active' : null}><a href="#user" onClick={() => props.changeGalleryType('USER', props.viral)}>User</a></li>
         </ul>
       </div>
     </nav>
@@ -22,16 +22,18 @@ const Menu = props => (
 Menu.propTypes = {
   galleryType: PropTypes.oneOf(['HOT', 'TOP', 'USER']),
   changeGalleryType: PropTypes.func,
+  viral: PropTypes.bool,
 };
 
 const mapStateToProps = state => ({
   galleryType: state.gallery.type,
+  viral: state.gallery.viral,
 });
 
 const mapDispatchToProps = dispatch => ({
-  changeGalleryType: (type) => {
+  changeGalleryType: (type, viral) => {
     dispatch(Actions.changeGalleryType(type));
-    dispatch(Actions.fetchGallery(type, 0, null));
+    dispatch(Actions.fetchGallery(type, 0, viral));
   },
 });
 
